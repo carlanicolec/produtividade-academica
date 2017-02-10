b3bb72b21da01c5560f8780bf0391c281927c5ed
Carla Nicole Calheiros Pimentel <carlanicole@ic.ufal.br>
2017-02-10 11:24:34 -0300

    finalização funcionalidades

--- src/base/Funcionalidades.java
+++ src/base/Funcionalidades.java
@@ -1,8 +1,11 @@
 package base;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
-public class Funcionalidades {
+public class Funcionalidades extends Exception{
     Scanner scan = new Scanner(System.in);
     Projeto novo_projeto = new Projeto();
     Professor novo_prof = new Professor();
@@ -56,25 +59,29 @@ public class Funcionalidades {
 
     public void addAlunoProjeto(int id_projeto, Dados dados) {
         for (int i = 0; i < dados.todos_projetos.size(); i++) {
-            if (dados.todos_projetos.get(i).getId_projeto() == id_projeto && dados.todos_projetos.get(i).getStatus() == 1) {
-                System.out.println("Nome do aluno:");
-                String nome_aluno = scan.nextLine();
-                System.out.println("Email do aluno:");
-                String email_aluno = scan.nextLine();
-                System.out.println("CPF do aluno:");
-                int cpf = Integer.parseInt(scan.nextLine());
-                System.out.println("Insira a opção desejada:\n" +
-                        "1 - Aluno de graduação\n" +
-                        "2 - Aluno de mestrado\n" +
-                        "3 - Aluno de doutorado\n");
+            if (dados.todos_projetos.get(i).getId_projeto() == id_projeto) {
+                if (dados.todos_projetos.get(i).getStatus() == 1) {
+                    System.out.println("Nome do aluno:");
+                    String nome_aluno = scan.nextLine();
+                    System.out.println("Email do aluno:");
+                    String email_aluno = scan.nextLine();
+                    System.out.println("CPF do aluno:");
+                    int cpf = Integer.parseInt(scan.nextLine());
+                    System.out.println("Insira a opção desejada:\n" +
+                            "1 - Aluno de graduação\n" +
+                            "2 - Aluno de mestrado\n" +
+                            "3 - Aluno de doutorado\n");
 
-                int tipo_aluno = Integer.parseInt(scan.nextLine());
+                    int tipo_aluno = Integer.parseInt(scan.nextLine());
 
-                Aluno novo_aluno = new Aluno(nome_aluno, email_aluno, tipo_aluno, cpf);
+                    Aluno novo_aluno = new Aluno(nome_aluno, email_aluno, tipo_aluno, cpf);
 
-                dados.todos_projetos.get(i).addAluno(novo_aluno);
-                novo_aluno.addNovoProjeto(dados.todos_projetos.get(i));
-                dados.todos_usuarios.add(novo_aluno);
+                    dados.todos_projetos.get(i).addAluno(novo_aluno);
+                    novo_aluno.addNovoProjeto(dados.todos_projetos.get(i));
+                    dados.todos_usuarios.add(novo_aluno);
+                }else{
+                    System.out.println("Projeto não está na fase de elaboração. Não podem ser feitas alterações\n");
+                }
             }
         }
     }
@@ -109,22 +116,24 @@ public class Funcionalidades {
     public void addOrientadorProjeto(int id_projeto, Dados dados, Orientador orientador) {
         for (int i = 0; i < dados.todos_projetos.size(); i++) {
 
-            if (dados.todos_projetos.get(i).getId_projeto() == id_projeto && dados.todos_projetos.get(i).getStatus() == 1) {
-                Projeto temp = dados.todos_projetos.get(i);
-                System.out.println("Nome do Orientador:\n");
-                String nome_orientador = scan.nextLine();
-                System.out.println("Email do Orientador:\n");
-                String email_orientador = scan.nextLine();
-                System.out.println("CPF do Orientador:");
-                int cpf = Integer.parseInt(scan.nextLine());
-                orientador.setNome(nome_orientador);
-                orientador.setEmail(email_orientador);
-                orientador.setCpf(cpf);
-                temp.addOrientador(orientador);
-                orientador.addNovoProjeto(dados.todos_projetos.get(i));
-                dados.todos_usuarios.add(orientador);
-            }else if (dados.todos_projetos.get(i).getStatus() != 1){
-                System.out.println("Projeto não está na fase de elaboração, logo não podem ser feitas alterações\n");
+            if (dados.todos_projetos.get(i).getId_projeto() == id_projeto) {
+                if (dados.todos_projetos.get(i).getStatus() == 1) {
+                    Projeto temp = dados.todos_projetos.get(i);
+                    System.out.println("Nome do Orientador:\n");
+                    String nome_orientador = scan.nextLine();
+                    System.out.println("Email do Orientador:\n");
+                    String email_orientador = scan.nextLine();
+                    System.out.println("CPF do Orientador:");
+                    int cpf = Integer.parseInt(scan.nextLine());
+                    orientador.setNome(nome_orientador);
+                    orientador.setEmail(email_orientador);
+                    orientador.setCpf(cpf);
+                    temp.addOrientador(orientador);
+                    orientador.addNovoProjeto(dados.todos_projetos.get(i));
+                    dados.todos_usuarios.add(orientador);
+                }else {
+                    System.out.println("Projeto não está na fase de elaboração. Não podem ser feitas alterações\n");
+                }
             }
         }
        // dados.printOrientadoresProjeto(1);
@@ -139,7 +148,7 @@ public class Funcionalidades {
                     dados.todos_projetos.get(i).alterarStatus(2);
                     System.out.println("Status alterado para 'Em andamento'");
                 } else if (status == 2) {
-                    if (dados.todos_projetos.get(i).getPublicacoes_projeto() != null) {
+                    if (dados.todos_projetos.get(i).getPublicacoes_projeto().size() > 0) {//verificar por size
                         System.out.println("Insira a data de hoje: ");
                         String data_termino = scan.nextLine();
                         dados.todos_projetos.get(i).setData_termino(data_termino);
@@ -181,6 +190,7 @@ public class Funcionalidades {
             System.out.println("Informe o CPF do autor:\n");
             int cpf = Integer.parseInt(scan.nextLine());
             autor.setCpf(cpf);
+            dados.todos_usuarios.add(autor);
         }
         Publicacoes nova_publicacao = new Publicacoes(titulo,conferencia, ano_publicacao, autor);
         System.out.println("Deseja atrelar a Publicação a um projeto existente?\n 1 - Sim\n 2 - Não\n");
@@ -189,9 +199,14 @@ public class Funcionalidades {
             System.out.println("Informe o id do projeto\n");
             int id = Integer.parseInt(scan.nextLine());
             for (int i = 0; i < dados.todos_projetos.size(); i++) {
-                if (dados.todos_projetos.get(i).getId_projeto() == id && dados.todos_projetos.get(i).getStatus() == 1) {
-                    dados.todos_projetos.get(i).addPublicacao(nova_publicacao);
-                    System.out.println("Publicação adcionada com sucesso\n");
+                if (dados.todos_projetos.get(i).getId_projeto() == id) {
+                    if (dados.todos_projetos.get(i).getStatus() == 2){
+                        dados.todos_projetos.get(i).addPublicacao(nova_publicacao);
+                        System.out.println("Publicação adcionada com sucesso\n");
+                    }else{
+                        System.out.println("Projeto não está na fase 'em andamento'. Não podem ser feitas alterações\n");
+                    }
+
                 }
             }
 
@@ -199,7 +214,7 @@ public class Funcionalidades {
 
         autor.addPublicacao(nova_publicacao);
         dados.todas_publicacoes.add(nova_publicacao);
-        dados.todos_usuarios.add(autor);
+
     }
 
     public void relatorios(Dados dados){
@@ -209,7 +224,7 @@ public class Funcionalidades {
                 "3 - Número de projetos em andamento\n" +
                 "4 - Número de projetos concluidos\n" +
                 "5 - Número total de projetos\n" +
-                "6 - Número de produção acadêmica por tipo de produção\n");
+                "6 - Número de Publicações\n");
 
         int escolha = Integer.parseInt(scan.nextLine());
         if (escolha == 1) {
@@ -244,40 +259,33 @@ public class Funcionalidades {
         } else if (escolha == 5) {
             System.out.println("Número total de projetos" + dados.todos_projetos.size());
         } else if (escolha == 6) {
-            System.out.println("Escolha o tipo de produção desejada:\n 1 - Publicações\n 2 - Orientações\n");
-            int choice = Integer.parseInt(scan.nextLine());
-            if (choice == 1) { //publicacoes
-                System.out.println("Número total de publicações" + dados.todas_publicacoes.size());
-            } else if (choice == 2) {
-
+            System.out.println("Número total de publicações" + dados.todas_publicacoes.size());
             }
         }
-    }
 
     public void consultas(Dados dados){
         System.out.println("Escolha a opção de consulta desejada:\n"+
                             "1 - Consulta por colaborador\n"+
                             "2 - Consulta por projeto");
         int opcao = Integer.parseInt(scan.nextLine());
+
         if (opcao == 1){
             System.out.println("Insira o CPF do colaboraborador:");
             int cpf = Integer.parseInt(scan.nextLine());
             for (int i = 0; i<dados.todos_usuarios.size(); i++){
                 if (dados.todos_usuarios.get(i).getCpf() == cpf){
                     Usuario colaborador = dados.todos_usuarios.get(i);
-                    System.out.println("Nome do colaborador: " + colaborador.getNome());
-                    System.out.println("Email do colaborador: " + colaborador.getEmail());
-                    System.out.println("Projetos:\n");
+                    System.out.println("Nome do colaborador: " + colaborador.getNome()+"\n");
+                    System.out.println("Email do colaborador: " + colaborador.getEmail()+"\n");
+                    System.out.println("Projetos:");
                     for (int j = 0; j<colaborador.getProjetos().size(); j++){
-                        System.out.println("Projeto " + j+1 +
-                                            "Título do projeto " + colaborador.getProjetos().get(j).getTitulo()+
-                                            "Data de término "+ colaborador.getProjetos().get(j).getData_termino());
+                        System.out.println( "Título do projeto " + colaborador.getProjetos().get(j).getTitulo()+"\n"+
+                                            "Data de término "+ colaborador.getProjetos().get(j).getData_termino()+"\n");
                     }
-                    System.out.println("Publicações\n");
+                    System.out.println("Publicações:");
                     for (int b = 0; b<colaborador.getPublicacoes().size(); b++){
-                        System.out.println("Publicação " + b+1 +
-                                            "Título da publicação " + colaborador.getPublicacoes().get(b).getTitulo()+
-                                            "Ano da publicação " + colaborador.getPublicacoes().get(b).getAno_publicacao());
+                        System.out.println( "Título da publicação " + colaborador.getPublicacoes().get(b).getTitulo()+"\n"+
+                                            "Ano da publicação " + colaborador.getPublicacoes().get(b).getAno_publicacao()+"\n");
                     }
                 }
             }
@@ -288,37 +296,34 @@ public class Funcionalidades {
             for (int i = 0; i<dados.todos_projetos.size(); i++){
                 if (dados.todos_projetos.get(i).getId_projeto() == id){
                     Projeto projeto = dados.todos_projetos.get(i);
-                    System.out.println("ID:  " + projeto.getId_projeto()+
-                                        "Título: " + projeto.getTitulo() +
-                                        "Data de início: " + projeto.getData_inicio()+
-                                        "Data de término: " + projeto.getData_termino()+
-                                        "Agencia financiadora: " + projeto.getAgencia_financiadora()+
-                                        "Valor financiado: " + projeto.getValor_financiado()+
-                                        "Objetivo: " + projeto.getObjetivo()+
-                                        "Descrição: "+ projeto.getDescricao()+
-                                        "Status: " + projeto.getStatus());
+                    System.out.println("ID:  " + projeto.getId_projeto()+"\n"+
+                                        "Título: " + projeto.getTitulo() +"\n"+
+                                        "Data de início: " + projeto.getData_inicio()+"\n"+
+                                        "Data de término: " + projeto.getData_termino()+"\n"+
+                                        "Agencia financiadora: " + projeto.getAgencia_financiadora()+"\n"+
+                                        "Valor financiado: " + projeto.getValor_financiado()+"\n"+
+                                        "Objetivo: " + projeto.getObjetivo()+"\n"+
+                                        "Descrição: "+ projeto.getDescricao()+"\n"+
+                                        "Status: " + projeto.getStatus()+"\n");
 
                     System.out.println("Alunos:\n");
                     for (int j = 0; j<projeto.getAlunos_participantes().size(); j++){
-                        System.out.println("Aluno " + j+1);
-                        System.out.println("Nome: " + projeto.getAlunos_participantes().get(j).getNome()+
-                                           "Email " + projeto.getAlunos_participantes().get(j).getEmail()+
-                                            "CPF " + projeto.getAlunos_participantes().get(j).getCpf());
+                        System.out.println("Nome: " + projeto.getAlunos_participantes().get(j).getNome()+"\n"+
+                                           "Email " + projeto.getAlunos_participantes().get(j).getEmail()+"\n"+
+                                            "CPF " + projeto.getAlunos_participantes().get(j).getCpf()+"\n");
 
                     }
                     System.out.println("Orientadores:\n");
                     for (int b = 0; b<projeto.getOrientadores().size(); b++){
-                        System.out.println("Orientador " + b+1);
-                        System.out.println("Nome: "+ projeto.getOrientadores().get(b).getNome() +
-                                            "Email "+ projeto.getOrientadores().get(b).getEmail()+
-                                            "CPF " + projeto.getOrientadores().get(b).getCpf());
+                        System.out.println("Nome: "+ projeto.getOrientadores().get(b).getNome() + "\n"+
+                                            "Email "+ projeto.getOrientadores().get(b).getEmail()+"\n"+
+                                            "CPF " + projeto.getOrientadores().get(b).getCpf()+"\n");
                     }
                     System.out.println("Publicações\n");
                     for (int n = 0; n<projeto.getPublicacoes_projeto().size(); n++){
-                        System.out.println("Publicação "+ n+1);
-                        System.out.println("Título "+ projeto.getPublicacoes_projeto().get(n).getTitulo()+
-                                            "Conferência "+ projeto.getPublicacoes_projeto().get(n).getConferencia()+
-                                            "Ano de Publicação"+ projeto.getPublicacoes_projeto().get(n).getAno_publicacao());
+                        System.out.println("Título "+ projeto.getPublicacoes_projeto().get(n).getTitulo()+"\n"+
+                                            "Conferência "+ projeto.getPublicacoes_projeto().get(n).getConferencia()+"\n"+
+                                            "Ano de Publicação"+ projeto.getPublicacoes_projeto().get(n).getAno_publicacao()+"\n");
                     }
 
                 }
@@ -327,5 +332,22 @@ public class Funcionalidades {
 
 
     }
+
+    public static Date formataData(String data) throws Exception {
+        Date date = null;
+        DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+        date = (java.util.Date)formatter.parse(data);
+        return date;
+    }
+
+    public void compare(Date date1, Date date2) {
+        if(date1.compareTo(date2) == -1)
+            return 1;
+        if(date1.compareTo(date2) == 1)
+            return -1;
+        else
+            return 0;
+    }
+
 
 }
